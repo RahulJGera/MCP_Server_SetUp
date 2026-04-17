237b02f3b1144b5c92e93a1d0e7586034c6913ea
Rahul Gera <rj0204g@gmail.com>
2026-04-17 02:16:33 +0000

    [introMCP] initial uv files with 0 to 5 packages/modules.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
+    print("Open_AI_Key=", os.getenv("OPENAI_API_KEY"))
+    print("Claude_AI_Key=", os.getenv("CLAUDE_API_KEY"))
+    print("LangSmith_AI_Key=", os.getenv("LANGSMITH_API_KEY"))
     print("Hello from mcp-server-setup!")
 
 
