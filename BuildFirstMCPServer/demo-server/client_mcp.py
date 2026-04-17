bb0631a899329916bd430d9f899feb743ed96371
Rahul Gera <rj0204g@gmail.com>
2026-04-17 04:21:06 +0000

    [firstMCPServer-Client-MCP] Files, content and commands that needs to run for MCP client using python-sdk.

---
+++ BuildFirstMCPServer/demo-server/client_mcp.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from mcp import ClientSession
+from mcp.client.streamable_http import streamablehttp_client
+
+async def client_main():
+    url = "http://127.0.0.1:8000/mcp/"
+    async with streamablehttp_client(url) as (read, write, get_session_id):
+        async with ClientSession(read, write) as session:
+            print("Before initialization: ", get_session_id())
+
+            await session.initialize()
+
+            sid = get_session_id()
+            print("After initialization: ", sid)
+
+            result = await session.call_tool("add", {"a": 21, "b": 2})
+            print("Result: ", result)
+
+if __name__ == "__main__":
+    asyncio.run(client_main())
--- COMMANDS.md
+++ COMMANDS.md
@@ -2,3 +2,4 @@ pip3 install uv
 uv add fastmcp dotenv mcp
 cd BuildFirstMCPServer/demo-server/
 uv run ./server.py
+uv run ./client_mcp.py
