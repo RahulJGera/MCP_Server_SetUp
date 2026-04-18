7242af89405cc4183107cd9615ac1ad309dac5aa
Rahul Gera <rj0204g@gmail.com>
2026-04-18 00:17:24 +0000

    [multiMCPServers] Added math_server, mcp-json.json MCP server, and the LLM client prompt.

---
+++ MCPServerAndLocalAgent/multiMCPServer/math_server.py
@@ -0,0 +1,32 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP(name="CalculatorServer")
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Adds two integers together."""
+    return a + b
+
+@mcp.tool
+def subract(a: int, b: int) -> int:
+    """Subtracts two integers."""
+    return abs(a - b)
+
+@mcp.tool
+def multiply(a: int, b: int) -> int:
+    """Multiply two integers."""
+    return a * b
+
+@mcp.tool
+def divide(a: int, b: int) -> int:
+    """Division of first from the second."""
+    return a // b
+
+@mcp.tool
+def expo(a: int, b: int) -> int:
+    """Exponential of first integer."""
+    
+    return a // b
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
--- MCPServerAndLocalAgent/weather-server/advanced_agentic_client.py
+++ MCPServerAndLocalAgent/weather-server/advanced_agentic_client.py
@@ -5,23 +5,25 @@ from mcp_use import MCPAgent, MCPClient
 async def main():
 
     # Create MCPClient from configuration JSON file.
-    client = MCPClient("./mcp-json.json")
+    client = MCPClient("/home/rj0402g/Documents/Learning/Practice/MCP_Server_SetUp/MCPServerAndLocalAgent/weather-server/mcp-json.json")
     # client = MCPClient()
 
     # Create LLM
     llm =ChatOllama(base_url="http://localhost:11434", model="qwen3.5")
+    # llm =ChatOllama(base_url="http://localhost:11434", model="llama3.1:8b")
 
     # Create agent with restricted tools
     agent = MCPAgent(
         llm=llm,
         client=client,
         max_steps=30,
+        use_server_manager=True,
         disallowed_tools=["file_system", "network"]  # Restrict potentially dangerous tools
     )
 
     # Run the query
     result = await agent.run(
-        "What is the current weather in Santa Clara, California and the three day forecast, if possible",
+        "What is the current weather in Santa Clara, California and the three day forecast, if possible. Also tell me the exponential of two with power of five.",
     )
     print(f"\nResult: {result}")
 
